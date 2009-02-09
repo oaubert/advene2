b5f71a0fef008abe3e1c851bd744f787d42c12bc
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2009-02-09 11:09:30 +0000

    gui.views.timeline: fix imports
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene2@5917 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- advene/gui/views/timeline.py
+++ advene/gui/views/timeline.py
@@ -28,8 +28,10 @@ from gettext import gettext as _
 # Advene part
 import advene.core.config as config
 
-from advene.model.schema import AnnotationType, RelationType
-from advene.model.annotation import Annotation, Relation
+from advene.model.cam.tag import RelationType
+from advene.model.cam.annotation import Annotation
+from advene.model.cam.tag import AnnotationType
+from advene.model.cam.relation import Relation
 from advene.gui.views import AdhocView
 import advene.gui.edit.elements
 from advene.gui.edit.create import CreateElementPopup
