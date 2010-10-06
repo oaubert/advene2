79f06f07d97e74b84971f152bbec76ae759b1837
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2010-10-06 14:01:43 +0000

    gui.views.finder: fix autoscroll
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene2@6912 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- advene/gui/views/finder.py
+++ advene/gui/views/finder.py
@@ -785,7 +785,7 @@ class Finder(AdhocView):
 
         # Scroll the columns
         adj=self.sw.get_hadjustment()
-        adj.value = adj.upper - .1
+        adj.value = adj.upper - adj.page_size
         return True
 
     def scroll_event(self, widget=None, event=None):
