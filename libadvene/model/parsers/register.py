26926cfffe76b78a490fe963a63efb68a8032d59
pachampin <pachampin@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2014-08-18 14:21:21 +0000

    made rdflib optional
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene2@7637 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- libadvene/model/parsers/register.py
+++ libadvene/model/parsers/register.py
@@ -3,8 +3,14 @@ import libadvene.model.parsers.advene_zip as advene_zip_parser
 import libadvene.model.parsers.cinelab_json as cinelab_json_parser
 import libadvene.model.parsers.cinelab_xml as cinelab_xml_parser
 import libadvene.model.parsers.cinelab_zip as cinelab_zip_parser
-import libadvene.model.parsers.cinelab_rdf as cinelab_rdf_parser
-import libadvene.model.parsers.cinelab_ttl as cinelab_ttl_parser
+try:
+    import rdflib
+    import libadvene.model.parsers.cinelab_rdf as cinelab_rdf_parser
+    import libadvene.model.parsers.cinelab_ttl as cinelab_ttl_parser
+except ImportError:
+    rdflib = None
+    from warnings import warn
+    warn("rdflib not available, could not register RDF parsers")
 
 
 # parser register functions
@@ -32,5 +38,6 @@ register_parser(advene_zip_parser.Parser)
 register_parser(cinelab_json_parser.Parser)
 register_parser(cinelab_xml_parser.Parser)
 register_parser(cinelab_zip_parser.Parser)
-register_parser(cinelab_rdf_parser.Parser)
-register_parser(cinelab_ttl_parser.Parser)
+if rdflib:
+    register_parser(cinelab_rdf_parser.Parser)
+    register_parser(cinelab_ttl_parser.Parser)
--- libadvene/model/serializers/register.py
+++ libadvene/model/serializers/register.py
@@ -3,8 +3,14 @@ import libadvene.model.serializers.advene_zip as advene_zip_serializer
 import libadvene.model.serializers.cinelab_json as cinelab_json_serializer
 import libadvene.model.serializers.cinelab_xml as cinelab_xml_serializer
 import libadvene.model.serializers.cinelab_zip as cinelab_zip_serializer
-import libadvene.model.serializers.cinelab_rdf as cinelab_rdf_serializer
-import libadvene.model.serializers.cinelab_ttl as cinelab_ttl_serializer
+try:
+    import rdflib
+    import libadvene.model.serializers.cinelab_rdf as cinelab_rdf_serializer
+    import libadvene.model.serializers.cinelab_ttl as cinelab_ttl_serializer
+except ImportError:
+    rdflib = None
+    from warnings import warn
+    warn("rdflib not available, could not register RDF serializers")
 
 # serializer register functions
 
@@ -31,5 +37,6 @@ register_serializer(advene_zip_serializer)
 register_serializer(cinelab_json_serializer)
 register_serializer(cinelab_xml_serializer)
 register_serializer(cinelab_zip_serializer)
-register_serializer(cinelab_rdf_serializer)
-register_serializer(cinelab_ttl_serializer)
+if rdflib:
+    register_serializer(cinelab_rdf_serializer)
+    register_serializer(cinelab_ttl_serializer)
